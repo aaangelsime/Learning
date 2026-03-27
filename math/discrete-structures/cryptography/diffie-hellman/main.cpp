e23a58e91432e02099c008961ed4c880555d4519
Angel <aasimental@icloud.com>
2026-03-27 16:58:23 -0600

    adding the struc of the rep, plus a mini project

---
+++ math/discrete-structures/cryptography/diffie-hellman/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void welcome();
+
+int main()
+{
+
+    return 0;
+}
+
+void welcome()
+{
+    std::ifstream welcomeFile("welcome.txt");
+
+    
+}
