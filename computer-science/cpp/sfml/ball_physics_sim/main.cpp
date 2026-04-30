fbd3339f2f1972c2089eb9570d1e0c240df1f5c2
Angel <aasimental@icloud.com>
2026-04-29 19:16:19 -0600

    new physics simulator project

---
+++ computer-science/cpp/sfml/ball_physics_sim/main.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+
+/*
+
+Steps for compiling new projects:
+mkdir build
+cd build
+cmake ..
+make 
+./MyProject
+
+Steps to compile:
+make 
+./MyProject
+
+*/
+
+int main()
+{
+    
+    return 0;
+}
